462c61bbf8a56ebf93f9998806fe572f47c6ff6b
DEBORAJ ROY <deborajroy123@gmail.com>
2025-11-16 01:36:16 +0600

    v10

---
+++ src/StockDamage/StockDamage.Web/Migrations/20251115193538_table_voucher.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StockDamage.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class table_voucher : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "VoucherNo",
+                table: "StockDamage",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "VoucherNo",
+                table: "StockDamage",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
